190888fa0a2ff4b2311cd47f7ca7b42c38f6da84
keshavmishra21 <keshavm@iitbhilai.ac.in>
2024-11-12 20:50:36 +0530

    Question-4

---
+++ 4a.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+FILE=$1
+
+if [[ ! -f "$FILE" ]]; then
+    echo "File not found!"
+    exit 1
+fi
+
+sex_0=0
+sex_1=0
+
+while IFS=, read -r id sex other; do
+    if [[ "$id" == "ID" ]]; then
+        continue
+    fi
+ 
+    if [[ "$sex" -eq 1 ]]; then
+        ((sex_1++))
+    elif [[ "$sex" -eq 0 ]]; then
+        ((sex_0++))
+    fi
+done < "$FILE"
+
+echo "0 $sex_0" > sex_count.dat
+echo "1 $sex_1" >> sex_count.dat
+
+gnuplot << EOF
+set terminal png
+set output '4a.png'
+ 
+set boxwidth 0.5
+set style fill solid 0.5
+set xlabel "Sex"
+set ylabel "Count"
+set title "Histogram of Sex vs Count"
+set xtics ("Female" 0,"Male" 1)
+plot 'sex_count.dat' using 2:xtic(1) with boxes notitle lc "red"
+EOF
+ 
+echo "Histogram saved as plot-4a.png"
