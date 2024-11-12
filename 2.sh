67d70bde0b1f0ee686d5707ae3af6c334be4da7d
keshavmishra21 <keshavm@iitbhilai.ac.in>
2024-11-12 23:44:13 +0530

    Added question-5 figures in Question-2's Latex file

--- 2.sh
+++ 2.sh
@@ -12,7 +12,41 @@ echo "\\documentclass{article}" > $output_file
 echo "\\usepackage{booktabs}" >> $output_file
 echo "\\usepackage{graphicx}" >> $output_file
 echo "\\usepackage{longtable}" >> $output_file
+echo "\\usepackage{float}" >> $output_file
 echo "\\begin{document}" >> $output_file
+
+
+echo "\\begin{figure}[H]" >> $output_file
+echo "\\centering" >> $output_file
+echo "\\includegraphics[width=0.7\\textwidth]{4a.png}" >> $output_file
+echo "\\caption{Gender vs Number}" >> $output_file
+echo "\\label{fig:sample_figure1}" >> $output_file
+echo "\\end{figure}" >> $output_file
+
+echo "\\begin{figure}[H]" >> $output_file
+echo "\\centering" >> $output_file
+echo "\\includegraphics[width=0.7\\textwidth]{4b.png}" >> $output_file
+echo "\\caption{Age vs Blood Pressure}" >> $output_file
+echo "\\label{fig:sample_figure2}" >> $output_file
+echo "\\end{figure}" >> $output_file
+
+echo "\\begin{figure}[H]" >> $output_file
+echo "\\centering" >> $output_file
+echo "\\includegraphics[width=0.7\\textwidth]{4c.png}" >> $output_file
+echo "\\caption{Age vs Cholestrol(No Heart Disease)}" >> $output_file
+echo "\\label{fig:sample_figure3}" >> $output_file
+echo "\\end{figure}" >> $output_file
+
+echo "\\begin{figure}[H]" >> $output_file
+echo "\\centering" >> $output_file
+echo "\\includegraphics[width=0.7\\textwidth]{4d.png}" >> $output_file
+echo "\\caption{Pie chart of Heart Disease}" >> $output_file
+echo "\\label{fig:sample_figure4}" >> $output_file
+echo "\\end{figure}" >> $output_file
+
+
+echo "Figure~\\ref{fig:sample_figure1} shows Historgram for Gender vs Number of Disease. Figure~\\ref{fig:sample_figure2} shows Age vs Blood Pressure. Figure~\\ref{fig:sample_figure3} shows Age vs Cholestrol(No Heart Disease). Figure~\\ref{fig:sample_figure4} shows Pie Chart for Heart Diseases." >> $output_file
+
 
 echo "\\begin{center}" >> $output_file
 
